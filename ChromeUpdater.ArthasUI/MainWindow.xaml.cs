986887d4d035b47a520dab95492fa5b3246d751d
wangpeiyang <wpyok500@gmail.com>
2022-02-16 10:03:30 +0800

    添加项目文件。

---
+++ ChromeUpdater.ArthasUI/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Arthas.Controls.Metro;
+using ChromeUpdater.Services;
+
+namespace ChromeUpdater.ArthasUI
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            //https://www.google.com/chrome/browser/thankyou.html?standalone=1&installdataindex=defaultbrowser&platform=win64&extra=stablechannel
+            //https://www.google.com/chrome/thank-you.html?standalone=1&statcb=1&installdataindex=empty&defaultbrowser=0
+            var menu = sender as MetroMenuItem;
+            if (menu == null) return;
+            var header = (string)menu.Header;
+            if (header.StartsWith("耍下"))
+                System.Diagnostics.Process.Start("https://github.com/shuax");
+            else if (header.StartsWith("ONEO"))
+                System.Diagnostics.Process.Start("https://github.com/1217950746");
+            else if (header.StartsWith("win_stable_x64"))
+                System.Diagnostics.Process.Start("https://www.google.com/chrome/thank-you.html?extra=stablechannel&platform=win64&standalone=1&statcb=1&installdataindex=empty&defaultbrowser=0");
+            else if (header.StartsWith("win_beta_x64"))
+                System.Diagnostics.Process.Start("https://www.google.com/chrome/thank-you.html?extra=betachannel&platform=win64&standalone=1&statcb=1&installdataindex=empty&defaultbrowser=0");
+            else if (header.StartsWith("win_dev_x64"))
+                System.Diagnostics.Process.Start("https://www.google.com/chrome/dev/thank-you.html?extra=devchannel&platform=win64&standalone=1&statcb=1&installdataindex=empty&defaultbrowser=0");
+            else if (header.StartsWith("win_canary_x64"))
+                System.Diagnostics.Process.Start("https://www.google.com/chrome/thank-you.html?extra=canarychannel&platform=win64&standalone=1&statcb=1&installdataindex=empty&defaultbrowser=0");
+            else if (header.StartsWith("Debian/Ubuntu"))
+                System.Diagnostics.Process.Start("https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb");
+            else if (header.StartsWith("Fedora/openSUSE"))
+                System.Diagnostics.Process.Start("https://dl.google.com/dl/linux/direct/google-chrome-stable_current_x86_64.rpm");
+            else if (header.StartsWith("mac_x64"))
+                System.Diagnostics.Process.Start("https://www.google.com/chrome/browser/thankyou.html?platform=mac&statcb=1&extra=stablechannel");
+
+            else if (header.StartsWith("Chromium"))
+                //http://commondatastorage.googleapis.com/chromium-browser-continuous/index.html
+                System.Diagnostics.Process.Start("http://commondatastorage.googleapis.com/chromium-browser-continuous/index.html");
+            else if (header.StartsWith("Chrome首页"))
+                //http://commondatastorage.googleapis.com/chromium-browser-continuous/index.html
+                System.Diagnostics.Process.Start("https://www.google.com/intl/zh-CN/chrome/");
+            else
+                System.Diagnostics.Process.Start("https://github.com/TkYu/ChromeUpdater");
+        }
+
+        private void TxtPath_OnButtonClick(object sender, EventArgs e)
+        {
+            ((ChromeUpdaterCore)DataContext).CmdFolderBrowse.Execute(null);
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            //init messageservice here
+            ServiceManager.Instance.AddService<IMessageService>(new MessageService(this));
+        }
+    }
+}
---
+++ ChromeUpdaterCore/Models/GCUpdateObject.cs
@@ -0,0 +1,29 @@
+﻿namespace ChromeUpdater
+{
+    public class GreenChromeUpdate
+    {
+        public float verison { get; set; }//WTF???
+        public string version { get; set; }
+        public string description { get; set; }
+        public GreenChromeWithArch link { get; set; }
+    }
+
+    public class GreenChromeWithArch
+    {
+        public GreenChrome x86 { get; set; }
+        public GreenChrome x64 { get; set; }
+    }
+
+    public class GreenChrome
+    {
+        public string url { get; set; }
+        public string sha1 { get; set; }
+
+        public string GetFileName()
+        {
+            var split = url.Split('/');
+            var last = split[split.Length - 1];
+            return last.Split('?')[0];
+        }
+    }
+}
